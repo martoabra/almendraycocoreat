48047810c9c64da3d033d414ffe14ca6a705402c
martoabra <martoo96@gmail.com>
2022-02-01 21:03:16 -0300

    Updates

--- src/context/Types.js
+++
@@ -1,5 +0,0 @@
-export const SHOW_HIDE_CART = 'SHOW_HIDE_CART'
-export const ADD_TO_CART = 'ADD_TO_CART'
-export const REST_TO_CART = 'REST_TO_CART' 
-export const VACIAR_CART = 'VACIAR_CART' 
-export const REMOVE_ITEM = 'REMOVE_ITEM' 
--- src/context/cart/CartContext.js
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react";
-import { Children } from "react/cjs/react.production.min";
-
-const CartContext = createContext()
-
-export default CartContext
